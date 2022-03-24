23b6945327e296bcd56bbdcdbe8641741bfc2fd7
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2022-03-24 10:35:26 -0500

    Conexion con backend

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -2,93 +2,30 @@ import React, { useState, useEffect } from "react";
 import style from "./Quiz.module.css";
 import { useParams } from "react-router-dom";
 
-const Quiz = () => {
-  const quiz = [
-    {
-      category: "General Knowledge",
-      question: "Nutella is produced by the German company Ferrero.",
-      options: [
-        {
-          answer: "true",
-          isCorrect: true,
-        },
-        {
-          answer: "false",
-          isCorrect: false,
-        },
-      ],
-    },
-    {
-      category: "General Knowledge",
-      question: "The vapor produced by e-cigarettes is actually water.",
-      options: [
-        {
-          answer: "true",
-          isCorrect: false,
-        },
-        {
-          answer: "false",
-          isCorrect: true,
-        },
-      ],
-    },
-    {
-      category: "General Knowledge",
-      question: "The sum of all the numbers on a roulette wheel is 666.",
-      options: [
-        {
-          answer: "true",
-          isCorrect: true,
-        },
-        {
-          answer: "false",
-          isCorrect: false,
-        },
-      ],
-    },
-    {
-      category: "General Knowledge",
-      question: "An eggplant is a vegetable.",
-      options: [
-        {
-          answer: "false",
-          isCorrect: true,
-        },
-        {
-          answer: "true",
-          isCorrect: false,
-        },
-      ],
-    },
-  ];
-
+const Quiz = ({ quiz }) => {
   const { category } = useParams();
   const actualURL = `/play/${category}`;
-
   const [actualQuestion, setActualQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [isFinished, setIsFinished] = useState(false);
-  const [timeRemaining, setTimeRemaining] = useState(10);
   const [areDisabled, setAreDisabled] = useState(false);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (timeRemaining > 0) setTimeRemaining((prev) => prev - 1);
-      if (timeRemaining === 0) setAreDisabled(!areDisabled);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [timeRemaining]);
+  const [responses, setResponses] = useState({});
 
   const handleAnswerSubmit = (isCorrect, e) => {
     if (isCorrect) setScore(score + 1);
+
+    console.log(typeof e.target.value);
+    setResponses({
+      ...responses,
+      [e.target.name]: e.target.value,
+    });
 
     e.target.classList.add(
       isCorrect ? `${style.correct}` : `${style.incorrect}`
     );
 
     setTimeout(() => {
-      if (actualQuestion === quiz.length - 1) {
+      if (quiz && actualQuestion === quiz.length - 1) {
         setIsFinished(true);
       } else {
         e.target.classList.remove(
@@ -113,6 +50,7 @@ const Quiz = () => {
           {" "}
           Play again
         </button>
+        <button className={style.gameOverContainer__button}>Save score</button>
       </div>
     );
 
@@ -124,30 +62,16 @@ const Quiz = () => {
             Question {actualQuestion + 1} of {quiz.length}
           </span>
           <div className={style.questionTitle}>
-            {quiz[actualQuestion].question}
+            {quiz && quiz[actualQuestion].question}
           </div>
-          {!areDisabled ? (
-            <span className={style.timeRemaining}>
-              Time remaining: {timeRemaining}
-            </span>
-          ) : (
-            <button
-              className={style.continueButton}
-              onClick={() => {
-                setTimeRemaining(10);
-                setAreDisabled(false);
-                setActualQuestion(actualQuestion + 1);
-              }}
-            >
-              Continue
-            </button>
-          )}
         </div>
         <div className={style.rightSide}>
           {quiz[actualQuestion].options.map((answer, index) => (
             <button
               key={index + 1}
               disabled={areDisabled}
+              name={quiz[actualQuestion].question}
+              value={answer.answer}
               className={style.optionButton}
               onClick={(e) => handleAnswerSubmit(answer.isCorrect, e)}
             >
--- src/Pages/Game/Game.jsx
+++ src/Pages/Game/Game.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import Quiz from "../../Components/Quiz/Quiz";
 import style from "./Game.module.css";
+const axios = require("axios");
 
 const Game = () => {
   const { category } = useParams();
+  //Elimino espacios y convierto en minusculas
+  category.toLowerCase().replace(/\s+/g, "");
+
+  const REQUEST_URL = `http://localhost:3001/api/questions/${category}`;
+
+  const [quiz, setQuiz] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    try {
+      axios.get(REQUEST_URL).then(({ data }) => {
+        setQuiz(data);
+        setIsLoading(!isLoading);
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  }, []);
+
+  if (isLoading) return <h1>Cargando...</h1>;
+
   return (
     <div className={style.gameContainer}>
       <h1 className={style.gameContainer__title}>{category} Quiz</h1>
-      <Quiz />
+      {quiz ? <Quiz quiz={quiz.quiz} /> : null}
       <Link className={style.gameContainer__backLink} to="/home">
         Back
       </Link>

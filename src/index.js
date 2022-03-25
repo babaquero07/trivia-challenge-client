0bfb5a348a561bdfc5b36be2c4f3287e9e0c3aa3
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2022-03-25 09:49:59 -0500

    Fix deploy

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 const root = document.getElementById("root");
 
